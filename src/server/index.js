4d89a8968cc593ff54ae4a644f1f6aa88a3c0004
AustinBahng <abahng@usc.edu>
2019-03-04 20:47:55 -0800

    temp disable url encoding

--- src/server/index.js
+++ src/server/index.js
@@ -7,7 +7,7 @@ import jwt from "jsonwebtoken";
 const PORT = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
+//app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", routes);
 
