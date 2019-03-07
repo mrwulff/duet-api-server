9bad6b42b227c1cafb54cac319bd6132bcf95ee6
AustinBahng <abahng@usc.edu>
2019-03-07 00:35:55 -0800

    modified donate fulfillment to handle list of item ids

--- src/server/controller/donate.js
+++ src/server/controller/donate.js
@@ -5,36 +5,38 @@ const conn = db.dbInitConnect();
 function fulfillNeed(req, res) {
   let body = req.body;
   if (body.itemId) {
-    // set item to fulfilled
-    conn.execute(
-      "UPDATE items SET is_fulfilled=true WHERE item_id=?",
-      [body.itemId],
-      function(err) {
-        if (err) {
-          console.log(err);
-        } else {
-          // add entry into donations table
-          conn.execute(
-            "INSERT INTO donations (timestamp,donor_fname,donor_lname,donor_email,donor_phone,donation_amt_usd) " +
-              " VALUES (NOW(),?,?,?,?,?)",
-            [
-              body.firstName,
-              body.lastName,
-              body.email,
-              body.phoneNumber,
-              body.amount
-            ],
-            function(err) {
-              if (err) {
-                console.log(err);
-              } else {
-                res.status(200).send();
+    body.itemId.forEach(function(id) {
+      // set item to fulfilled
+      conn.execute(
+        "INSERT INTO donations (timestamp,donor_fname,donor_lname,donor_email,donor_phone,donation_amt_usd) " +
+          " VALUES (NOW(),?,?,?,?,?)",
+        [
+          body.firstName,
+          body.lastName,
+          body.email,
+          body.phoneNumber,
+          body.amount
+        ],
+        function(err) {
+          if (err) {
+            console.log(err);
+          } else {
+            // add entry into donations table
+            conn.execute(
+              "UPDATE items SET is_fulfilled=true,donation_id=SELECT LAST_INSERT_ID() WHERE item_id=?",
+              [id],
+              function(err) {
+                if (err) {
+                  console.log(err);
+                } else {
+                  res.status(200).send();
+                }
               }
-            }
-          );
+            );
+          }
         }
-      }
-    );
+      );
+    });
   }
 }
 
