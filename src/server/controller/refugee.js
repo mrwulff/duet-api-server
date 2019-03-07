f5cc7b1d5450932cc779329b2ca4633dc4a5087f
austinbahng <abahng@usc.edu>
2019-03-06 22:00:00 -0800

    wip beneficiary endpoints

--- src/server/controller/refugee.js
+++ src/server/controller/refugee.js
@@ -36,118 +36,127 @@ function getNeeds(req, res) {
   //     res.json(rows);
   //   }
   // );
-  res.json([
-    {
-      id: 398,
-      name: "Almasi",
-      origin: "Damascus, Syria",
-      currentResettled: "Munich, Germany",
-      familyImage:
-        "https://www.rescue.org/sites/default/files/styles/window_width_breakpoints_theme_rescue_large_1x/public/article/1488/teaser/dsc_0997-edit.jpg?itok=GsB-pi9g&timestamp=1499184376",
-      needs: [
-        {
-          icon: "http://cdn.onlinewebfonts.com/svg/img_472233.png",
-          blurb: "Clothes for Winter"
-        },
-        {
-          icon:
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTePAHJnsC-yJuZ_m5_HU0Jh_p7qp-RcZFl91PkYMjRBNjz1UpcHA",
-          blurb: "Textbooks for children's education"
-        },
-        {
-          icon: "https://static.thenounproject.com/png/88457-200.png",
-          blurb: "Kettle for making Tea"
-        }
-      ]
-    },
-    {
-      id: 399,
-      name: "Omid",
-      origin: "Kabul, Afghanistan",
-      currentResettled: "Munich, Germany",
-      familyImage:
-        "http://cdn3.spiegel.de/images/image-1172522-860_poster_16x9-urwv-1172522.jpg",
-      needs: [
-        {
-          icon: "http://cdn.onlinewebfonts.com/svg/img_472233.png",
-          blurb: "Clothes for Winter"
-        },
-        {
-          icon:
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTePAHJnsC-yJuZ_m5_HU0Jh_p7qp-RcZFl91PkYMjRBNjz1UpcHA",
-          blurb: "Textbooks for children's education"
-        },
-        {
-          icon: "https://static.thenounproject.com/png/88457-200.png",
-          blurb: "Kettle for making Tea"
-        }
-      ]
-    },
-    {
-      id: 400,
-      name: "Khan",
-      origin: "Tehran, Iran",
-      currentResettled: "Lesvos, Greece",
-      familyImage:
-        "https://www.680news.com/wp-content/blogs.dir/sites/2/2015/09/15/HAL102_20150915298586_hd.jpg",
-      needs: [
-        {
-          icon: "http://cdn.onlinewebfonts.com/svg/img_472233.png",
-          blurb: "Clothes for Winter"
-        },
-        {
-          icon:
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTePAHJnsC-yJuZ_m5_HU0Jh_p7qp-RcZFl91PkYMjRBNjz1UpcHA",
-          blurb: "Textbooks for children's education"
-        },
-        {
-          icon: "https://static.thenounproject.com/png/88457-200.png",
-          blurb: "Kettle for making Tea"
-        }
-      ]
-    }
-  ]);
+  // res.json([
+  //   {
+  //     id: 398,
+  //     name: "Almasi",
+  //     origin: "Damascus, Syria",
+  //     currentResettled: "Munich, Germany",
+  //     familyImage:
+  //       "https://www.rescue.org/sites/default/files/styles/window_width_breakpoints_theme_rescue_large_1x/public/article/1488/teaser/dsc_0997-edit.jpg?itok=GsB-pi9g&timestamp=1499184376",
+  //     needs: [
+  //       {
+  //         icon: "http://cdn.onlinewebfonts.com/svg/img_472233.png",
+  //         blurb: "Clothes for Winter"
+  //       },
+  //       {
+  //         icon:
+  //           "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTePAHJnsC-yJuZ_m5_HU0Jh_p7qp-RcZFl91PkYMjRBNjz1UpcHA",
+  //         blurb: "Textbooks for children's education"
+  //       },
+  //       {
+  //         icon: "https://static.thenounproject.com/png/88457-200.png",
+  //         blurb: "Kettle for making Tea"
+  //       }
+  //     ]
+  //   },
+  //   {
+  //     id: 399,
+  //     name: "Omid",
+  //     origin: "Kabul, Afghanistan",
+  //     currentResettled: "Munich, Germany",
+  //     familyImage:
+  //       "http://cdn3.spiegel.de/images/image-1172522-860_poster_16x9-urwv-1172522.jpg",
+  //     needs: [
+  //       {
+  //         icon: "http://cdn.onlinewebfonts.com/svg/img_472233.png",
+  //         blurb: "Clothes for Winter"
+  //       },
+  //       {
+  //         icon:
+  //           "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTePAHJnsC-yJuZ_m5_HU0Jh_p7qp-RcZFl91PkYMjRBNjz1UpcHA",
+  //         blurb: "Textbooks for children's education"
+  //       },
+  //       {
+  //         icon: "https://static.thenounproject.com/png/88457-200.png",
+  //         blurb: "Kettle for making Tea"
+  //       }
+  //     ]
+  //   },
+  //   {
+  //     id: 400,
+  //     name: "Khan",
+  //     origin: "Tehran, Iran",
+  //     currentResettled: "Lesvos, Greece",
+  //     familyImage:
+  //       "https://www.680news.com/wp-content/blogs.dir/sites/2/2015/09/15/HAL102_20150915298586_hd.jpg",
+  //     needs: [
+  //       {
+  //         icon: "http://cdn.onlinewebfonts.com/svg/img_472233.png",
+  //         blurb: "Clothes for Winter"
+  //       },
+  //       {
+  //         icon:
+  //           "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTePAHJnsC-yJuZ_m5_HU0Jh_p7qp-RcZFl91PkYMjRBNjz1UpcHA",
+  //         blurb: "Textbooks for children's education"
+  //       },
+  //       {
+  //         icon: "https://static.thenounproject.com/png/88457-200.png",
+  //         blurb: "Kettle for making Tea"
+  //       }
+  //     ]
+  //   }
+  // ]);
 }
 
 function getFamilyInfo(req, res) {
-  res.json({
-    beneficiaryId: 398,
-    name: "Almasi",
-    story: "",
-    originCity: "Damascus",
-    originCountry: "Syria",
-    currentCity: "Munich",
-    currentCountry: "Germany",
-    familyImage:
-      "https://www.rescue.org/sites/default/files/styles/window_width_breakpoints_theme_rescue_large_1x/public/article/1488/teaser/dsc_0997-edit.jpg?itok=GsB-pi9g&timestamp=1499184376",
-    needs: [
-      {
-        itemId: 1,
-        image:
-          "https://cdn.shopify.com/s/files/1/1143/7052/products/1_167caef8-830d-45e8-80ea-92e1d7167d13_600x.png?v=1518105578",
-        name: "Coat",
-        price: 17.0,
-        fulfilled: false,
-        storeId: 1
-      },
-      {
-        itemId: 2,
-        image:
-          "https://i.pinimg.com/originals/68/5d/61/685d61a47e631a12cef4156a62cf2557.jpg",
-        name: "Mathematics",
-        price: 24.0,
-        fulfilled: false,
-        storeId: 2
-      },
-      {
-        itemId: 3,
-        image: "https://www.chantal.com/images/D/init_image896.jpg",
-        name: "Kettle",
-        price: 10.0,
-        fulfilled: false,
-        storeId: 3
+  let beneficiaryId = req.query.beneficiary_id;
+  let query =
+    "SELECT CONCAT(beneficiaries.first_name, ' ', beneficiaries.last_name) as 'beneficiary_name', story, " +
+    "origin_city, origin_country, current_city, current_country, family_image_url " +
+    "FROM beneficiaries";
+  if (req.query.beneficiaryId) {
+    query += " WHERE beneficiary_id = ?";
+  }
+
+  conn.execute("", [beneficiaryId], function(err, rows) {
+    if (err) {
+      console.log(err);
+    }
+    let beneficiaryObj = {
+      beneficiaryId: beneficiaryId,
+      name: rows[0].beneficiary_name,
+      story: rows[0].story,
+      originCity: rows[0].origin_city,
+      originCountry: rows[0].origin_country,
+      currentCity: rows[0].current_city,
+      currentCountry: rows[0].currentCountry,
+      familyImage: rows[0].family_image_url
+    };
+    conn.execute(
+      "SELECT item_id, display_link, name, price_euros, is_fulfilled, store_id FROM items WHERE beneficiary_id = ?",
+      [beneficiaryId],
+      function(err, rows) {
+        if (err) {
+          console.log(err);
+        }
+        let item;
+        let needs = [];
+        rows.forEach(function(obj) {
+          item = {
+            itemId: obj.item_id,
+            image: obj.display_link,
+            name: obj.name,
+            price: obj.price_euros,
+            fulfilled: obj.is_fulfilled,
+            storeId: obj.store_id
+          };
+          needs.push(item);
+        });
+        beneficiaryObj["needs"] = needs;
+        res.json(beneficiaryObj);
       }
-    ]
+    );
   });
 }
 
