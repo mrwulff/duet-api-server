6c00b92a04c140f84a8b8c4733c1737918d3d7fc
AustinBahng <abahng@usc.edu>
2019-03-09 00:42:37 -0800

    reenable urlencoded

--- src/server/controller/donate.js
+++ src/server/controller/donate.js
@@ -4,7 +4,6 @@ const conn = db.dbInitConnect();
 
 function fulfillNeed(req, res) {
   let body = req.body;
-  console.log(req);
   if (body.itemIds) {
     // set item to fulfilled
     conn.execute(
