453c36d8f834ea830c860eb60d2136b1e167dd1c
AustinBahng <abahng@usc.edu>
2019-03-09 00:01:12 -0800

    attempt to fix cors bug

--- src/server/index.js
+++ src/server/index.js
@@ -12,8 +12,9 @@ app.use(express.urlencoded({ extended: true }));
 
 // enable CORS
 app.use(function(req, res, next) {
+  console.log("ENABLING CORS");
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
