cd765e5c466f57218ae1f7f0a73711087c3cf4e9
AustinBahng <abahng@usc.edu>
2019-03-09 00:36:02 -0800

    deploy

---
+++ dist/controller/donate.js
@@ -0,0 +1,48 @@
+"use strict";Object.defineProperty(exports, "__esModule", { value: true });exports.default = void 0;var _config = _interopRequireDefault(require("./../config/config.js"));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}
+
+var conn = _config.default.dbInitConnect();
+
+function fulfillNeed(req, res) {
+  var body = req.body;
+  console.log(req);
+  if (body.itemIds) {
+    // set item to fulfilled
+    conn.execute(
+    "INSERT INTO donations (timestamp,donor_fname,donor_lname,donor_email,donor_phone,donation_amt_usd) " +
+    " VALUES (NOW(),?,?,?,?,?)",
+    [
+    body.firstName,
+    body.lastName,
+    body.email,
+    body.phoneNumber,
+    body.amount],
+
+    function (err) {
+      if (err) {
+        console.log(err);
+        res.status(400).send();
+      } else {
+        body.itemIds.forEach(function (id) {
+          // add entry into donations table
+          conn.execute(
+          "UPDATE items SET is_fulfilled=true,donation_id=(SELECT LAST_INSERT_ID()) WHERE item_id=?",
+          [id],
+          function (err) {
+            if (err) {
+              console.log(err);
+              res.status(400).send();
+            } else {
+              res.status(200).send();
+            }
+          });
+
+        });
+      }
+    });
+
+  } else {
+    res.status(400).json();
+  }
+}var _default =
+
+{ fulfillNeed: fulfillNeed };exports.default = _default;
---
+++ dist/routes/donate.js
@@ -0,0 +1,10 @@
+"use strict";Object.defineProperty(exports, "__esModule", { value: true });exports.default = void 0;var _express = _interopRequireDefault(require("express"));
+var _donate = _interopRequireDefault(require("./../controller/donate"));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}
+
+var router = _express.default.Router();
+
+router.post("/fulfill", function (req, res) {
+  _donate.default.fulfillNeed(req, res);
+});var _default =
+
+router;exports.default = _default;
