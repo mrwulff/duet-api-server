c4e6cf13ad50f81b0acdee91e6090826bb3bf03a
austinbahng <abahng@usc.edu>
2019-03-20 18:44:38 -0700

    testing typeform

--- dist/controller/refugee.js
+++ dist/controller/refugee.js
@@ -3,7 +3,7 @@
 var conn = _config.default.dbInitConnect();
 
 function processTypeform(req, res) {
-  var body = req.body.form_response.definition.answers;
+  var body = req.body.form_response.definition;
   console.log(body);
   res.status(200).send();
 }
