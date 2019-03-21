146d59c133894e05355d644848a2c0c070623dbf
austinbahng <abahng@usc.edu>
2019-03-20 18:29:15 -0700

    add dist

---
+++ dist/routes/refugeeProtected.js
@@ -0,0 +1,11 @@
+"use strict";Object.defineProperty(exports, "__esModule", { value: true });exports.default = void 0;var _express = _interopRequireDefault(require("express"));
+var _refugee = _interopRequireDefault(require("./../controller/refugee"));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}
+
+var router = _express.default.Router();
+var app = (0, _express.default)();
+
+router.post("/typeform", function (req, res) {
+  _refugee.default.processTypeform(req, res);
+});var _default =
+
+router;exports.default = _default;
