97f66f4331f54dd05c0f745d82dcc0d4faa4bf8e
lucashu1 <lucashu@usc.edu>
2019-04-08 16:28:44 -0700

    Config typo

--- src/server/controller/donate.js
+++ src/server/controller/donate.js
@@ -11,7 +11,7 @@ const conn = db.dbInitConnect();
 var paypalConfig = {
   'mode': process.env.PAYPAL_MODE,
   'client_id': process.env.PAYPAL_CLIENT_ID,
-  'client_secret': process.env.PAYPAL_CLIENT_SERVER
+  'client_secret': process.env.PAYPAL_CLIENT_SECRET
 };
 var paypal = require('paypal-rest-sdk');
 paypal.configure(paypalConfig);
