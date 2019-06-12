d16ff8e31cbf2b48edadc336abc18884c0dff256
Raghav Maheshwari <maheshwr@usc.edu>
2019-06-12 11:06:27 -0700

    use capitalized config variable for cron interval

--- dist/controller/donate.js
+++ dist/controller/donate.js
@@ -261,7 +261,7 @@ function testDBConnection(req, res) {
 
 // CRON job to send notification email to storeowner every day at 8:00 AM if there are
 // novel items to that (1) need price approval or (2) need to be picked up.
-new CronJob(process.env.cronInterval, function () {
+new CronJob(process.env.CRON_INTERVAL, function () {
   console.log('running cron job checking if stores need to be notified...');
   sendStoreownerNotificationEmail();
 }, null, true, 'America/Los_Angeles');
