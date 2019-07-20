4706ae220ecef141b57cf9e90a0fe1bae309e495
Lucas Hu <lucashu@usc.edu>
2019-07-20 12:04:10 -0700

    In sandbox, only send donor thank you to duet.giving (#32)
    * Generic item status update email
    
    * donor thank you email: in sandbox, only send to duet.giving

--- src/server/util/sendgridHelpers.js
+++ src/server/util/sendgridHelpers.js
@@ -28,7 +28,7 @@ async function sendDonorThankYouEmail(donorInfo) {
     // Send donor thank-you email
     // Takes in donorInfo object with "email", "firstName" fields
     const msg = {
-      to: ["duet.giving@gmail.com", donorInfo.email],
+      to: (process.env.SENDGRID_NOTIFICATION_BEHAVIOR === "live") ? ["duet.giving@gmail.com", donorInfo.email] : "duet.giving@gmail.com",
       from: "duet@giveduet.org",
       templateId: "d-2780c6e3d4f3427ebd0b20bbbf2f8cfc",
       dynamic_template_data: {
