48e30b6795b41c8a97c65ae0eb44881bdbee736f
Lucas Hu <lucashu@usc.edu>
2019-08-28 14:44:29 -0700

    Auto-rotate images before s3 upload (when exif data exists) (#45)

--- src/server/util/s3Helpers.js
+++ src/server/util/s3Helpers.js
@@ -6,26 +6,51 @@ import errorHandler from './errorHandler.js';
 const rp = require('request-promise');
 const path = require('path');
 const mime = require('mime-types');
+const jo = require('jpeg-autorotate');
+import piexif from 'piexifjs';
+
+// see: https://www.npmjs.com/package/jpeg-autorotate#thumbnail-too-large
+function deleteThumbnailFromExif(imageBuffer) {
+    const imageString = imageBuffer.toString('binary');
+    const exifObj = piexif.load(imageString);
+    delete exifObj.thumbnail;
+    delete exifObj['1st'];
+    const exifBytes = piexif.dump(exifObj);
+    const newImageString = piexif.insert(exifBytes, imageString);
+    return Buffer.from(newImageString, 'binary');
+}
 
 async function uploadItemImageToS3(itemId, imageUrl) {
     try {
-        var options = {
-            uri: imageUrl,
-            encoding: null
-        };
         let extension = path.extname(imageUrl);
         let contentType = mime.contentType(extension);
-
-        let body = await rp(options);
+        var imageBuffer = await rp({
+            uri: imageUrl,
+            encoding: null
+        });
+        try {
+            console.log("Deleting thumbnail from exif...");
+            imageBuffer = deleteThumbnailFromExif(imageBuffer);
+            console.log("Rotating image...");
+            const joResult = await jo.rotate(imageBuffer, {
+                quality: 85
+            });
+            imageBuffer = joResult.buffer;
+            console.log("Successfully rotated image");
+        }
+        catch (err) {
+            console.log("jpeg-autorotate error: " + err);
+        }
+        console.log("Uploading image to s3...");
         let data = await s3.upload({
-            Body: body,
+            Body: imageBuffer,
             Key: process.env.AWS_S3_IMAGE_FOLDER + '/item-' + itemId + extension,
             Bucket: process.env.AWS_S3_BUCKET_NAME,
             ACL: "public-read",
             ContentType: contentType
         }).promise();
         let s3PhotoUrl = data.Location;
-        console.log("success uploading image to s3: " + s3PhotoUrl);
+        console.log("Success uploading image to s3: " + s3PhotoUrl);
         return s3PhotoUrl;
     } catch (err) {
         errorHandler.handleError(err, "s3Helpers/uploadItemImageToS3");
