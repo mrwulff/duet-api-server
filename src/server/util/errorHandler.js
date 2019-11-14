a33c58bc51ff0325359fd272586ed2b78176a5f6
Audrey Brooke <abrooke@flexport.com>
2019-11-14 10:31:35 -0800

    Slack Error Service Integration (#84)
    * Slack Error Service Integration
    
    * remove channel

--- src/server/util/errorHandler.js
+++ src/server/util/errorHandler.js
@@ -1,21 +1,38 @@
 import sendgridHelpers from "./sendgridHelpers.js";
 require("dotenv").config();
+const rp = require('request-promise');
 
-// Standard error handler: console log, and send us an email
-function handleError(err, functionName = false) {
+
+// Standard error handler: console log, and send us a slack message
+async function handleError(err, functionName = false) {
   try {
-    if (functionName) {
-      console.log("Error in " + functionName + ": " + err.stack);
-      if (process.env.SEND_ERROR_EMAILS && process.env.SEND_ERROR_EMAILS === 'true') {
-        sendgridHelpers.sendErrorEmail(err.toString(), functionName);
+      console.log(err.stack);
+
+      if (process.env.NODE_ENV == 'production') {
+        await rp({
+          method: 'POST',
+          uri: process.env.SLACK_PROD_ERROR_WEBHOOK,
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: {
+            text: err.stack
+          },
+          json: true
+        });
+      } else if (process.env.NODE_ENV == 'staging') {
+        await rp({
+          method: 'POST',
+          uri: process.env.SLACK_STAGING_ERROR_WEBHOOK,
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: {
+            text: err.stack
+          },
+          json: true
+        });
       }
-    }
-    else {
-      console.log("Error in unknown function: " + err.stack);
-      if (process.env.SEND_ERROR_EMAILS && process.env.SEND_ERROR_EMAILS === 'true') {
-        sendgridHelpers.sendErrorEmail(err.toString(), "unknownFunction");
-      }
-    }
   } catch (err) {
     console.log("Error in errorHandler/handleError (lol): " + err.toString());
   }
