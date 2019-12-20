5e321806df1b881ed481ced9793d8161526e7129
Lucas Hu <lucashu@usc.edu>
2019-12-20 01:07:44 -0800

    use sharp for image processing; convert item photos to jpg (#106)
    * use sharp for image processing; convert webp to jpg
    
    * use const instead of env var for s3 bucket

--- src/server/util/s3Helpers.js
+++ src/server/util/s3Helpers.js
@@ -6,56 +6,58 @@ import errorHandler from './errorHandler.js';
 const rp = require('request-promise');
 const path = require('path');
 const mime = require('mime-types');
-const jo = require('jpeg-autorotate');
-import piexif from 'piexifjs';
+const sharp = require('sharp');
 
-// see: https://www.npmjs.com/package/jpeg-autorotate#thumbnail-too-large
-function deleteThumbnailFromExif(imageBuffer) {
-  const imageString = imageBuffer.toString('binary');
-  const exifObj = piexif.load(imageString);
-  delete exifObj.thumbnail;
-  delete exifObj['1st'];
-  const exifBytes = piexif.dump(exifObj);
-  const newImageString = piexif.insert(exifBytes, imageString);
-  return Buffer.from(newImageString, 'binary');
+// S3 bucket settings
+const originalPhotosBucket = 'duet-web-assets'
+
+// jpeg settings
+const jpgQuality = 80;
+const jpgExtension = '.jpg'
+const jpgMimeType = mime.contentType(jpgExtension);
+
+async function processImageBuffer(imageBuffer) {
+  // convert to jpeg and auto-rotate
+  try {
+    const processedImageBuffer = await sharp(imageBuffer)
+      .jpeg({
+        quality: jpgQuality
+      })
+      .rotate()
+      .toBuffer();
+    return processedImageBuffer;
+  } catch (err) {
+    errorHandler.handleError(err, "s3Helpers/processImageBuffer");
+    throw err;
+  }
 }
 
 async function uploadItemImageToS3(itemId, imageUrl) {
   try {
-    const extension = path.extname(imageUrl);
-    const contentType = mime.contentType(extension);
+    // download image from Typeform
     let imageBuffer = await rp({
       uri: imageUrl,
       encoding: null
     });
-    try {
-      console.log("Deleting thumbnail from exif...");
-      imageBuffer = deleteThumbnailFromExif(imageBuffer);
-      console.log("Rotating image...");
-      const joResult = await jo.rotate(imageBuffer, {
-        quality: 85
-      });
-      imageBuffer = joResult.buffer;
-      console.log("Successfully rotated image");
-    }
-    catch (err) {
-      console.log("jpeg-autorotate error: " + err);
-    }
+
+    // image processing
+    imageBuffer = await processImageBuffer(imageBuffer);
+
+    // upload image to S3
     console.log("Uploading image to s3...");
     const data = await s3.upload({
       Body: imageBuffer,
-      Key: process.env.AWS_S3_IMAGE_FOLDER + '/item-' + itemId + extension,
-      Bucket: process.env.AWS_S3_BUCKET_NAME,
+      Key: process.env.AWS_S3_IMAGE_FOLDER + '/item-' + itemId + jpgExtension,
+      Bucket: originalPhotosBucket,
       ACL: "public-read",
-      ContentType: contentType
+      ContentType: jpgMimeType
     }).promise();
     const s3PhotoUrl = data.Location;
     console.log("Success uploading image to s3: " + s3PhotoUrl);
 
     // return image link hosted on AWS CDN (connected to S3 bucket)
-    const imageCDNLink = `${process.env.AWS_CDN}/${process.env.AWS_S3_IMAGE_FOLDER}/item-${itemId}${extension}`; 
+    const imageCDNLink = `${process.env.AWS_CDN}/${process.env.AWS_S3_IMAGE_FOLDER}/item-${itemId}${jpgExtension}`; 
     return imageCDNLink;
-
   } catch (err) {
     errorHandler.handleError(err, "s3Helpers/uploadItemImageToS3");
     throw err;
@@ -64,24 +66,29 @@ async function uploadItemImageToS3(itemId, imageUrl) {
 
 async function uploadPriceTagImageToS3(itemId, priceTagImageUrl) {
   try {
-    const extension = path.extname(priceTagImageUrl);
-    const contentType = mime.contentType(extension);
+    // download image from Typeform
     let imageBuffer = await rp({
       uri: priceTagImageUrl,
       encoding: null
     });
+
+    // image processing: convert to .jpg
+    imageBuffer = await processImageBuffer(imageBuffer);
+
+    // upload image to S3
     console.log("Uploading price-tag image to s3...");
     const data = await s3.upload({
       Body: imageBuffer,
-      Key: process.env.AWS_S3_IMAGE_FOLDER + '/item-' + itemId + '-price-tag' + extension,
-      Bucket: process.env.AWS_S3_BUCKET_NAME,
+      Key: process.env.AWS_S3_IMAGE_FOLDER + '/item-' + itemId + '-price-tag' + jpgExtension,
+      Bucket: originalPhotosBucket,
       ACL: "public-read",
-      ContentType: contentType
+      ContentType: jpgMimeType
     }).promise();
     const s3PhotoUrl = data.Location;
     console.log("Success uploading price-tag image to s3: " + s3PhotoUrl);
+
     // return image link hosted on AWS CDN (connected to S3 bucket)
-    const imageCDNLink = `${process.env.AWS_CDN}/${process.env.AWS_S3_IMAGE_FOLDER}/item-${itemId}-price-tag${extension}`;
+    const imageCDNLink = `${process.env.AWS_CDN}/${process.env.AWS_S3_IMAGE_FOLDER}/item-${itemId}-price-tag${jpgExtension}`;
     return imageCDNLink;
   } catch (err) {
     errorHandler.handleError(err, "s3Helpers/uploadPriceTagImageToS3");
